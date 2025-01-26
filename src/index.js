d59b4541dd4c9a7a9a52fe4d6385eee1bd9f9107
Mohamed Nagy <mohmmednagy@gmail.com>
2025-01-26 11:44:16 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 4000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1> nowjj! </h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
