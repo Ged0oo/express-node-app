c3c90131c69b45165bd4ce0478c8ed951f6a8a4a
Mohamed Nagy <mohmmednagy@gmail.com>
2025-01-28 08:36:08 +0200

    use mongodb and momgoose with the app container

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const port = 4000;
 const app = express();
 
+const mongoURI = 'mongodb://root:example@mongodb:27017';
+mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to DB"))
+    .catch((err) => console.log("Failed to connect to DB:", err)); // Fixed error logging
+
 app.get('/', (req, res) => {
-    res.send('<h1> hi dev! </h1>');
+    res.send('<h1>Hi Dev!</h1>');
 });
 
 app.listen(port, () => {
